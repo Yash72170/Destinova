332c2ae12ec31ac32e457a7761b569ef92a25096
Yashraj Shukla <125136048+Yash72170@users.noreply.github.com>
2025-09-25 16:22:38 +0530

    Update Footer.jsx

--- src/view-trip/components/Footer.jsx
+++ src/view-trip/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
   return (
     <div className='my-7'>
-        <h2 className='text-center text-gray-400'>Created with 🩷 by BroCoaders</h2>
+        <h2 className='text-center text-gray-400'>Created with 🩷 by Yashraj Shukla</h2>
       
     </div>
   )
